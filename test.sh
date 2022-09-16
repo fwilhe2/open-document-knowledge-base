3701777e54143df596b749575ec276453395947a
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-09-16 18:21:21 +0200

    Formulae and named expressions

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 mkdir -p output
 libreoffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":"44,34,76,1,,1031,true,true" test/minimal.fods --outdir output
+libreoffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":"44,34,76,1,,1031,true,true" test/formula.fods --outdir output
+libreoffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":"44,34,76,1,,1031,true,true" test/named-range.fods --outdir output
 
 cat output/*
