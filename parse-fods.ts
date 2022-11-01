3c27ad9e0b600b40fea7d60a162cee7118b8a9a5
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-11-01 21:11:14 +0100

    create html from fods

--- parse-fods.ts
+++ parse-fods.ts
@@ -12,15 +12,25 @@ let jsonObj = parser.parse(fodsString);
 
 const tableRows = jsonObj['office:document']['office:body']['office:spreadsheet']['table:table']['table:table-row']
 
-for (let rowIndex = 0; rowIndex < 2; rowIndex++) {
-    const currentRow = tableRows[rowIndex]
-    for (let cellIndex = 0; cellIndex < 3; cellIndex++) {
-        const currentCell = currentRow['table:table-cell'][cellIndex]
 
-        console.log(currentCell['@_office:value'] ? currentCell['@_office:value'] : currentCell['@_table:formula'])
+type formula = string
+type value = string
 
-    }
+type cell = {formula: formula} | {value: value}
 
+type row = cell[]
+type table = row[]
+
+function getCellText(c: any): string {
+    return c['@_office:value'] ? c['@_office:value'] : c['@_table:formula']
 }
 
+const tableJson = tableRows.map((r: any) => (r['table:table-cell'].map((c: any) => (getCellText(c)))))
+
+console.log(JSON.stringify(tableJson))
+
+const tableHtml = '<table>' + tableRows.map((r: any) => ('<tr>' + r['table:table-cell'].map((c: any) => (`<td> <input type="text" value="${getCellText(c)}"> </input> </td>`)).join('\n') + '</tr>')).join('\n') + '</table>'
+
+
 writeFileSync('formula.json', JSON.stringify(jsonObj, null, 4))
+writeFileSync('formula.html', tableHtml)
